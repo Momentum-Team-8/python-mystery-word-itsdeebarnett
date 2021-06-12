b8ab132cad41be2e587e808b47f3f9b18dabf9b9
itsdeebarnett <jdeebarnett@gmail.com>
2021-06-12 03:23:17 -0400

    starting to code

---
+++ mystery-word.py
@@ -0,0 +1,20 @@
+import random
+
+
+""" Create an variable for words or
+create a variable of words for each level?"""
+
+"""Easy mode only 4-6 characters Normal Mode 6-8
+characters Hard Mode 8+ characters"""
+
+"""At the start of the game tell users
+how many letters thats in the word"""
+
+"""The user gets one letter guess per round if they
+enter more than one tell user the input is invalid and try again"""
+
+
+print("\n Welcome to Mystery Word Game")
+print("\n Would you like to play a game?")
+
+word_file = open("words.txt", "r")
